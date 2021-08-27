dbf695b8ee2723d705f390f1493d4de6b7be54d8
SHINJON MUKHERJEE <82530706+Smukh123@users.noreply.github.com>
2021-08-27 13:36:02 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -98,7 +98,6 @@ document.onkeydown = function(e) {
 }
 
 <!--Start of Tawk.to Script-->
-<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
