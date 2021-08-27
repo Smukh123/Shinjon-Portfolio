e6c5b741db3eebd88b111265d368322f0494b401
SHINJON MUKHERJEE <82530706+Smukh123@users.noreply.github.com>
2021-08-27 13:52:24 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -97,18 +97,17 @@ document.onkeydown = function(e) {
   }
 }
 
-<!--Start of Tawk.to Script-->
+// Start of Tawk.to Live Chat
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/612898db649e0a0a5cd332fd/1fe39a7f9';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/612898db649e0a0a5cd332fd/1fe39a7f9';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+// End of Tawk.to Live Chat
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
