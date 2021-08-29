03cd2e3f923b3bb0cc09c34449d426710056aa19
SHINJON MUKHERJEE <82530706+Smukh123@users.noreply.github.com>
2021-08-29 17:04:11 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -21,11 +21,11 @@ document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
         document.title = "Projects | Portfolio Shinjon Mukherjee";
-        $("#favicon").attr("href","./assests/images/favicon.png");
+        $("#favicon").attr("href","https://raw.githubusercontent.com/Smukh123/Shinjon-Portfolio/main/assests/images/favicon.png");
     }
     else {
         document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","./assests/images/favhand.png");
+        $("#favicon").attr("href","https://raw.githubusercontent.com/Smukh123/Shinjon-Portfolio/main/assests/images/favhand.png");
     }
 });
 
